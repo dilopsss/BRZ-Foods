af948c4023b1453911ed7ec9aec10b75bfce666f
Diego Lopes <doplay1992@gmail.com>
2025-10-03 14:17:33 -0400

    feat: adicionar produtos Toddy, Nescau, Mucilon, Sazon e Yoki

--- lib/types.ts
+++ lib/types.ts
@@ -2,11 +2,19 @@ export type Product = {
   id: string
   name: string
   description: string
-  image: string
+
+  // pode ter só uma imagem…
+  image?: string
+
+  // …ou várias imagens
+  images?: string[]
+
   category: "Chocolates" | "Laticinios" | "Temperos" | "Cereais" | "Outros"
-  amazonUrl: string
+
+  amazonUrl?: string
   squareUrl?: string
-  price?: number
+
+  price: number
   rank?: number
   badge?: string
 }
